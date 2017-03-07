07798efb7b061c3118d316b5a8ffa002ee6b32bf
liuwenzhi11 <liuwenzhi1101@163.com>
2017-03-07 15:36:35 +0800

    Update TestGit.java

--- src/com/maxf/TestGit.java
+++ src/com/maxf/TestGit.java
@@ -4,6 +4,7 @@ public class TestGit {
 	
 	public static void main(String[] args) {
 		System.out.println(1);
+		System.out.println(0);
 		System.out.println(7);
 		System.out.println(5);
 		System.out.println(2);
