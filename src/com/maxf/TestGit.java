9495963533b499d2fe18f55f8c749283070ec72b
wenzhi.liu <wenzhi.liu@NBJWENZHI-LIU.msxf.com>
2017-04-10 17:11:21 +0800

    12

--- src/com/maxf/TestGit.java
+++ src/com/maxf/TestGit.java
@@ -16,6 +16,7 @@ public class TestGit {
 		System.out.println(4);
 		System.out.println(8);
 		System.out.println(8);
+		System.out.println(33);
 		System.out.println(9);
 		System.out.println(9);
 		System.out.println(10);
